be5216ada6d5828e1e6731dc770957e25824cc55
pushparajdev05 <pushparajdev05@gmail.com>
2024-05-21 16:55:30 +0530

    the border has deigned now later i wil do the rest of things

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget
   const MyApp({super.key}); 
   @override
   Widget build(BuildContext context) {
-    return const HomePage();
+    return const MaterialApp(
+      home : HomePage()
+    );
   }
 }
--- lib/page1.dart
+++ lib/page1.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget
 {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-      return const MaterialApp(
-      home:Scaffold(
+      return const Scaffold(
+        backgroundColor:Colors.blueGrey,
         body:Center(
-          child:Text("Text is here!!!")
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Text("0",
+              style:TextStyle(color:Colors.white,
+              fontSize:55,
+              fontWeight:FontWeight.bold)
+              ),
+              TextField(
+                style:TextStyle(
+                  color:Color.fromARGB(255, 0, 0, 0),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16
+                ),
+                decoration:InputDecoration(
+                hintText:"Please enter the value ",
+                hintStyle:TextStyle(color:Color.fromARGB(255, 0, 0, 0)),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor:Color.fromRGBO(0, 0, 0, 1),
+                filled:true,
+                fillColor:Colors.white,
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(
+                      color:Colors.black,
+                      width:2.0,
+                      style:BorderStyle.solid,
+                      strokeAlign:BorderSide.strokeAlignOutside,
+
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(60))
+                ),
+              )
+              )
+            ]
+          )
         )
 
-      )
-    );
+      );
+
   }
 
 }
